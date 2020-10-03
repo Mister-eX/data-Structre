f6b748e5c6af32c788f7d3450d42037bfb820069
Mister-eX <misterxx.ex@gmail.com>
2020-10-03 10:57:14 +0530

    trouck tour

---
+++ truck_tour.py
@@ -0,0 +1,13 @@
+def GasStationTour(gasstations):
+    N = len(gasstations)
+    start_point = 0
+    end_point = 1
+    curreent = gasstations[start_point][0] - gasstations[start_point][1]
+    
+    while curreent < 0 or start_point != end_point:
+        while curreent < 0 or start_point != end:
+            start_point  = (start_point + 1) % N
+            
+        curreent += gasstations[end][0] - gasstations[end][1]
+        end = (end + 1) % N
+    return start_point
